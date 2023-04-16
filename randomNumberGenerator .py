d8ff522afa10889b9821111fcf0ea9ca17184782
Nica Tudor <zatalo.nox@gmail.com>
2023-04-16 21:31:11 +0300

    Create file.py

---
+++ randomNumberGenerator .py
@@ -0,0 +1,15 @@
+#Importing a random number
+
+import random
+
+#Max number
+
+userMaxNumber = int(input("Enter the maximum number that can be generated: "))
+
+#Random number generator formula
+
+random_number = random.randint(0, userMaxNumber)
+
+#Displaying the random number
+
+print(random_number)
